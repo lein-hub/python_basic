67c00abdb43a815a67dd72a4b68a087366fdf177
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-09-25 18:08:55 +0900

    programmers

---
+++ programmers/counts_of_divisor_and_addition.py
@@ -0,0 +1,17 @@
+def solution(left, right):
+    result = 0
+    for i in range(left, right+1):
+        count = 1
+        for j in range(1, i//2 + 1):
+            if i % j == 0:
+                count += 1
+        print(count)
+        if count % 2 == 0:
+            result += i
+        else:
+            result -= i
+
+    return result
+
+
+print(solution(13, 17))
